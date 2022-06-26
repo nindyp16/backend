bfbd16008c07deba93d910a8fe26e449a3e7af77
Anindya Hilmi Pradipta <83213602+nindyp16@users.noreply.github.com>
2022-06-26 15:18:26 +0700

    Add files via upload

---
+++ backend/api/aimprove.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"backend/backend/repository"
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type AimproveListErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type ListAimprovecamp struct {
+	Id          string `json:"id"`
+	Id_User     int64  `json:"id_user"`
+	PilihanCamp string `json:"pilihan_camp"`
+	Motivasi    string `json:"motivasi"`
+}
+
+type AimproveListSuccessResponse struct {
+	Aimprove []ListAimprovecamp `json:"aimprove"`
+}
+
+func (a *API) getAimprove(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	response := AimproveListSuccessResponse{}
+	response.Aimprove = make([]ListAimprovecamp, 0)
+
+	aimprove, err := a.aimproveRepo.GetAll()
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			encoder.Encode(AimproveListErrorResponse{Error: err.Error()})
+		}
+	}()
+	if err != nil {
+		return
+	}
+	for _, b := range aimprove {
+		response.Aimprove = append(response.Aimprove, ListAimprovecamp{
+			Id:          strconv.Itoa(int(b.Id)),
+			Id_User:     int64(b.Id_User),
+			PilihanCamp: b.PilihanCamp,
+			Motivasi:    b.Motivasi,
+		})
+	}
+
+	encoder.Encode(response)
+}
+
+func (a *API) getAimproveById(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	response := AimproveListSuccessResponse{}
+	response.Aimprove = make([]ListAimprovecamp, 0)
+
+	id := r.URL.Query().Get("id")
+	idInt, err := strconv.Atoi(id)
+
+	aimprove, err := a.aimproveRepo.GetById(int64(idInt))
+	defer func() {
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			encoder.Encode(AimproveListErrorResponse{Error: err.Error()})
+		}
+	}()
+	if err != nil {
+		return
+	}
+	response.Aimprove = append(response.Aimprove, ListAimprovecamp{
+		Id:          strconv.Itoa(int(aimprove.Id)),
+		Id_User:     int64(aimprove.Id_User),
+		PilihanCamp: aimprove.PilihanCamp,
+		Motivasi:    aimprove.Motivasi,
+	})
+	encoder.Encode(response)
+
+}
+
+func (api *API) daftarAimprove(w http.ResponseWriter, r *http.Request) {
+	api.AllowOrigin(w, r)
+	var s ListAimprovecamp
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	create := repository.Aimprove{
+		Id_User:     s.Id_User,
+		PilihanCamp: s.PilihanCamp,
+		Motivasi:    s.Motivasi,
+	}
+	res, err := api.aimproveRepo.CreateAimprove(create)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
---
+++ backend/repository/aimprove.go
@@ -0,0 +1,64 @@
+package repository
+
+import "database/sql"
+
+type AimproveRepository struct {
+	db *sql.DB
+}
+
+func NewAimproveRepository(db *sql.DB) *AimproveRepository {
+	return &AimproveRepository{db: db}
+}
+
+func (r *AimproveRepository) GetAll() ([]Aimprove, error) {
+	var aimprove []Aimprove
+	rows, err := r.db.Query("SELECT * FROM aimprove")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var b Aimprove
+		err := rows.Scan(&b.Id, &b.Id_User, &b.Motivasi, &b.PilihanCamp)
+		if err != nil {
+			return nil, err
+		}
+		aimprove = append(aimprove, b)
+	}
+	return aimprove, nil
+}
+
+func (r *AimproveRepository) GetById(id int64) (Aimprove, error) {
+	var b Aimprove
+	err := r.db.QueryRow("SELECT * FROM aimprove WHERE id = ?", id).Scan(&b.Id, &b.Id_User, &b.Motivasi, &b.PilihanCamp)
+	if err != nil {
+		return b, err
+	}
+	return b, nil
+}
+
+func (r *AimproveRepository) CreateAimprove(b Aimprove) (int64, error) {
+	// sql statement yang digunakan untuk menambahkan data student baru
+	sqlStmt := `INSERT INTO aimprove (id, id_user, pilihan_camp, motivasi)
+		VALUES (?, ?, ?, ?);`
+
+	// membuat prepared statement dengan sql statement yang sudah dibuat
+	// dan mengirimkan parameter yang dibutuhkan
+	// untuk menambahkan data student baru
+	result, err := r.db.Exec(sqlStmt, b.Id, b.Id_User, b.PilihanCamp, b.Motivasi)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.LastInsertId()
+}
+
+// func (r *AimproveRepository) AddAimprove(id_user int, pilihan_camp string, motivasi string) (Aimprove, error) {
+// 	var s Aimprove
+// 	err := r.db.QueryRow("INSERT INTO aimprove (id, id_user, pilihan_camp, motivasi) VALUES (?, ?, ?, ?) RETURNING id, id_user, pilihan_camp, motivasi",
+// 		id_user, pilihan_camp, motivasi).Scan(&s.Id, &s.Id_User, &s.PilihanCamp, &s.Motivasi)
+// 	if err != nil {
+// 		return s, err
+// 	}
+// 	return s, nil
+// }
